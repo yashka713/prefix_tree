59cffb326ae2e8f408f3e075d7482616d6ea9928
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-21 18:40:15 +0200

    Divided find_or_create to find and create methods

--- tree.rb
+++ tree.rb
@@ -15,14 +15,18 @@ class Tree
 
   private
 
+  def find_or_create_sprout(branch, letter)
+    find_sprout(branch, letter) || create_sprout(branch, letter)
+  end
+
+  def find_sprout(branch, letter)
+    branch.children.each { |node| return node if node.char == letter }
+    nil
+  end
+
   def create_sprout(branch, letter)
     sprout = TreeNode.new(letter)
     branch.children << sprout
     sprout
-  end
-
-  def find_or_create_sprout(branch, letter)
-    branch.children.each { |node| return node if node.char == letter }
-    create_sprout(branch, letter)
   end
 end
