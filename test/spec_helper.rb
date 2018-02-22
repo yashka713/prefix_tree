212c8fa4071afa0d3ec0ff4608bacf36e30c5d3a
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-22 18:28:36 +0200

    Moved helper method to let in test

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -3,13 +3,6 @@ require 'byebug'
 load 'tree_node.rb'
 load 'tree.rb'
 
-WORDS = %w[cat cop cup can call chat chart clap cost].freeze
-
-def feel_tree(tree)
-  WORDS.each { |word| tree.add(word) }
-  tree
-end
-
 def find_my_first_branch(node, str)
   str << node.char
   return str if node.leaf
