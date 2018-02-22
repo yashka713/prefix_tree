c328354add54faf9a290baea9eef823300cc4b57
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-22 12:36:31 +0200

    Added test folder and changed before helper to let

---
+++ test/prefix_tree_spec.rb
@@ -0,0 +1,38 @@
+require_relative 'spec_helper'
+
+describe 'TreeNode' do
+  let(:tree_node) { TreeNode.new('a') }
+
+  it 'create TreeNode' do
+    tree_node.must_be_instance_of TreeNode
+    assert_equal(tree_node.instance_variable_get(:@char), 'a')
+  end
+
+  it 'should not create TreeNode for digit letter' do
+    err = -> { TreeNode.new }.must_raise ArgumentError
+    err.message.must_match 'wrong number of arguments'
+  end
+end
+
+describe 'Tree' do
+  let(:tree) { Tree.new }
+
+  it 'create Tree' do
+    tree.must_be_instance_of Tree
+    assert_equal(tree.instance_variable_get(:@node).instance_variable_get(:@char), '*')
+  end
+
+  it 'should not create Tree' do
+    err = -> { Tree.new('qwerty') }.must_raise ArgumentError
+    err.message.must_match 'wrong number of arguments'
+  end
+
+  it 'should add new word to Tree' do
+    assert_equal(tree.add('qwerty'), 'qwerty')
+    assert_equal(tree.add('qwerty1'), 'qwerty')
+    assert_equal(tree.add('213132qwe323212131rty1112131'), 'qwerty')
+    assert_equal(tree.add('    2131    32q   we3232   12131rty111  2131'), 'qwerty')
+    tree.instance_variable_get(:@node).must_be_instance_of TreeNode
+    assert_equal(tree.instance_variable_get(:@node).instance_variable_get(:@char), '*')
+  end
+end
