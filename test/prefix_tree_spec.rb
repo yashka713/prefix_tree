61fa06bc0b213a99110667f383c977cb60f244f5
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-22 13:41:21 +0200

    Added checking for first child in Tree

--- test/prefix_tree_spec.rb
+++ test/prefix_tree_spec.rb
@@ -19,6 +19,7 @@ describe 'Tree' do
 
   it 'create Tree' do
     tree.must_be_instance_of Tree
+    tree.instance_variable_get(:@node).must_be_instance_of TreeNode
     assert_equal(tree.instance_variable_get(:@node).instance_variable_get(:@char), '*')
   end
 
@@ -26,13 +27,32 @@ describe 'Tree' do
     err = -> { Tree.new('qwerty') }.must_raise ArgumentError
     err.message.must_match 'wrong number of arguments'
   end
+end
+
+describe 'Adding new word to Tree' do
+  let(:tree) { Tree.new }
+  let(:first_branch) do
+    mother_node = tree.instance_variable_get(:@node).instance_variable_get(:@children).first
+    find_my_first_branch(mother_node, '')
+  end
 
   it 'should add new word to Tree' do
-    assert_equal(tree.add('qwerty'), 'qwerty')
-    assert_equal(tree.add('qwerty1'), 'qwerty')
-    assert_equal(tree.add('213132qwe323212131rty1112131'), 'qwerty')
-    assert_equal(tree.add('    2131    32q   we3232   12131rty111  2131'), 'qwerty')
-    tree.instance_variable_get(:@node).must_be_instance_of TreeNode
-    assert_equal(tree.instance_variable_get(:@node).instance_variable_get(:@char), '*')
+    assert_equal(tree.add('qwerty'), 'qwerty added to Tree')
+    assert_equal('qwerty', first_branch)
+  end
+
+  it 'should remove all digits and add new word to Tree' do
+    assert_equal(tree.add('213132qwe323212131rty1112131'), 'qwerty added to Tree')
+    assert_equal('qwerty', first_branch)
+  end
+
+  it 'should remove all whitespaces and add new word to Tree' do
+    assert_equal(tree.add(' q w er t y '), 'qwerty added to Tree')
+    assert_equal('qwerty', first_branch)
+  end
+
+  it 'should remove all special symbols and add new word to Tree' do
+    assert_equal(tree.add('/q*w-e+r[t}y&'), 'qwerty added to Tree')
+    assert_equal('qwerty', first_branch)
   end
 end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,3 +1,10 @@
 require 'minitest/autorun'
+require 'byebug'
 load 'tree_node.rb'
 load 'tree.rb'
+
+def find_my_first_branch(node, str)
+  str << node.char
+  return str if node.leaf
+  find_my_first_branch(node.children.first, str)
+end
