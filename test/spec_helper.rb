326af37aa6f1aef9704fcac069f2324ccd21233f
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-26 23:50:11 +0200

    Redefine Tree::FILE_PATH for testing

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -9,7 +9,10 @@ def find_first_word_in_tree(node, str)
   find_first_word_in_tree(node.children.first, str)
 end
 
-TEST_FOLDER_PATH = File.join('data', 'test')
+TEST_FOLDER_PATH = File.join('test', 'data')
 
 Dir.mkdir('data') unless File.exist?('data')
 Dir.mkdir(TEST_FOLDER_PATH) unless File.exist?(TEST_FOLDER_PATH)
+# redefine Tree::FILE_PATH
+Tree.send(:remove_const, :FILE_PATH)
+Tree.const_set(:FILE_PATH, TEST_FOLDER_PATH)
