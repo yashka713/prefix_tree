5b3a4152401249404297c51ef447550431d2694e
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-26 12:01:08 +0200

    Rubocop check

--- tree.rb
+++ tree.rb
@@ -9,23 +9,22 @@ class Tree
     word = word.delete('^a-zA-Z')
     return 'Fill line with word, please' if word.empty?
     branch = @node
-    word.chars.each {|letter| branch = find_or_create_sprout(branch, letter)}
+    word.chars.each { |letter| branch = find_or_create_sprout(branch, letter) }
     branch.leaf = true
   end
 
   def includes?(word)
     branch = @node
-    word.delete('^a-zA-Z').chars.all? {|letter| branch = find_sprout(branch, letter)} && branch.leaf
+    word.delete('^a-zA-Z').chars.all? { |letter| branch = find_sprout(branch, letter) } && branch.leaf
   end
 
   def list
     @node.children.empty? ? 'Tree is empty' : list_v_1(@node, '', [])
     # @node.children.empty? ? 'Tree is empty' : list_v_2
-
   end
 
   def list_v_2(current_node = @node, temp_str = '', words_arr = [])
-    temp_str = temp_str + current_node.char unless current_node == @node
+    temp_str += current_node.char unless current_node == @node
     current_node.children.each do |child|
       words_arr << temp_str + child.char if child.leaf
       words_arr = list_v_2(child, temp_str, words_arr.clone)
@@ -36,9 +35,9 @@ class Tree
   private
 
   def list_v_1(node, str, tree)
-    str = str + node.char unless node == @node
+    str += node.char unless node == @node
     tree << str if node.leaf
-    node.children.each {|child_node| list_v_1(child_node, str, tree)}
+    node.children.each { |child_node| list_v_1(child_node, str, tree) }
     tree
   end
 
@@ -47,7 +46,7 @@ class Tree
   end
 
   def find_sprout(branch, letter)
-    branch.children.find {|node| node.char == letter}
+    branch.children.find { |node| node.char == letter }
   end
 
   def create_sprout(branch, letter)
