5545816ce7a0a4b2b07eb60ff516488ce21a5f12
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-02-27 11:59:49 +0200

    Changed folder name for tests data

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -9,7 +9,7 @@ def find_first_word_in_tree(node, str)
   find_first_word_in_tree(node.children.first, str)
 end
 
-TEST_FOLDER_PATH = File.join('test', 'data')
+TEST_FOLDER_PATH = File.join('test', 'support')
 
 Dir.mkdir('data') unless File.exist?('data')
 Dir.mkdir(TEST_FOLDER_PATH) unless File.exist?(TEST_FOLDER_PATH)
